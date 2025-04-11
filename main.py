c472ee49c5018ae556d678f2c4e870f784efab34
Benie Gouli <gouliaudrey@gmail.com>
2025-04-11 23:59:16 +0200

    You have set setup the display frame try to understand how to use the CTk font utility. find a better colour theme

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import customtkinter as ctk 
 
+class MyFrame(ctk.CTkFrame):
+    def __init__(self, root, width, height, fg_color, corner_radius= 0): # frames needs to be initialized with the master frame, which is the attribute root
+        super().__init__(root, width= width, height= height, fg_color= fg_color, corner_radius= corner_radius) # allows me to recreate a frame with custom attributes
+        self.text= ctk.CTkLabel(self, text=" Guess the number", text_color= "#fff", font=("Perfect Delight 1992.otf", 20)) 
+        #the font is not correctly set. You shoul use a CTkFont object
+        self.text.grid(row= 0, column= 0, padx=20, pady= 75, sticky= 'ew')
+
 class MainApp(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Numgy")
-        self.geometry("600x550+700+200")
+        self.geometry("500x550+700+200")
         self._set_appearance_mode("light")
+        self.textFrame = MyFrame(self, 400, 200, "#b3b6b7")
+        self.textFrame.grid_columnconfigure(0, weight= 1)
+        self.textFrame.grid(row= 0, column= 0, padx= 45, pady=10, sticky= "ew")
+  
 
 
 window = MainApp()
+window.grid_columnconfigure(0, weight= 1)
 window.mainloop()
