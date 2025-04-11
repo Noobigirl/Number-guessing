3020a2c0c5cdea8a029728fd7279fb65f1537311
Benie Gouli <gouliaudrey@gmail.com>
2025-04-12 00:06:35 +0200

    just realized that I do not need to set the fram size ( it takes the size of its content). so I remove the size attribute

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk 
 
 class MyFrame(ctk.CTkFrame):
-    def __init__(self, root, width, height, fg_color, corner_radius= 0): # frames needs to be initialized with the master frame, which is the attribute root
-        super().__init__(root, width= width, height= height, fg_color= fg_color, corner_radius= corner_radius) # allows me to recreate a frame with custom attributes
+    def __init__(self, root, fg_color, corner_radius= 0): # frames needs to be initialized with the master frame, which is the attribute root
+        super().__init__(root, fg_color= fg_color, corner_radius= corner_radius) # allows me to recreate a frame with custom attributes
         self.text= ctk.CTkLabel(self, text=" Guess the number", text_color= "#fff", font=("Perfect Delight 1992.otf", 20)) 
         #the font is not correctly set. You shoul use a CTkFont object
         self.text.grid(row= 0, column= 0, padx=20, pady= 75, sticky= 'ew')
@@ -13,7 +13,7 @@ class MainApp(ctk.CTk):
         self.title("Numgy")
         self.geometry("500x550+700+200")
         self._set_appearance_mode("light")
-        self.textFrame = MyFrame(self, 400, 200, "#b3b6b7")
+        self.textFrame = MyFrame(self, "#b3b6b7")
         self.textFrame.grid_columnconfigure(0, weight= 1)
         self.textFrame.grid(row= 0, column= 0, padx= 45, pady=10, sticky= "ew")
   
